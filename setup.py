1e84af0aad748015723e4bc5c3c5eadfdf9c47f8
Dror <hellfliers@walla.co.il>
2016-05-14 00:54:24 +0300

    fix pep8

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(name='euler',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
- 	  ]
-    )
+          ]
+      )
