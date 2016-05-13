8e531a348ca658bc5d306b534d5d29e5f7ed0797
Dror <hellfliers@walla.co.il>
2016-05-14 01:10:23 +0300

    change implementation to fit for solution paradigm

--- euler/euler25.py
+++ euler/euler25.py
@@ -3,15 +3,19 @@ import scipy.constants
 import numpy as np
 
 
-def fibonacci_digits_brute(digits):
-    pass
+def fibonacci_digits(digits):
+    """ Returns the amount of iterations required to get to 
+        the number of digits
 
-
-def fibonacci_digits_math(digits):
-    """ Returns the amount of iterations required to get to t
-        he number of digits
+	Input: Number of digits the fibonacci number should have
     """
     phi = scipy.constants.golden
     n = np.ceil((np.log(10) * (digits - 1) + np.log(5) / 2) / np.log(phi))
 
     return int(n)
+
+
+def solution():
+    """ Returns the answer as requested by the euler project problem """
+    required_digits = 1000
+    fibonacci_digits(required_digits)
--- tests/test_euler25.py
+++ tests/test_euler25.py
@@ -2,8 +2,8 @@ from euler import euler25 as fib_digits
 
 
 def test_fibonnnaci_digits_math_correct():
-    num_2digit = fib_digits.fibonacci_digits_math(2)
+    num_2digit = fib_digits.fibonacci_digits(2)
     assert num_2digit == 7
 
-    num_3digit = fib_digits.fibonacci_digits_math(2)
+    num_3digit = fib_digits.fibonacci_digits(3)
     assert num_3digit == 12
