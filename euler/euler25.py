16a78a0ae7c681e86340ebd6c2d67f08e17d8e82
Dror <hellfliers@walla.co.il>
2016-05-14 02:00:58 +0300

    fix pep8

--- euler/euler25.py
+++ euler/euler25.py
@@ -4,10 +4,10 @@ import numpy as np
 
 
 def fibonacci_digits(digits):
-    """ Returns the amount of iterations required to get to 
+    """ Returns the amount of iterations required to get to
         the number of digits
 
-	Input: Number of digits the fibonacci number should have
+        Input: Number of digits the fibonacci number should have
     """
     phi = scipy.constants.golden
     n = np.ceil((np.log(10) * (digits - 1) + np.log(5) / 2) / np.log(phi))
