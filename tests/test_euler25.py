f6a21ecfda48da9077e2e069adfac582fbfb3972
Dror <hellfliers@walla.co.il>
2016-05-14 00:48:39 +0300

    rename fibonacci to quesion number, change tox and yaml to use pytest correctly

---
+++ tests/test_euler25.py
@@ -0,0 +1,9 @@
+from euler import euler25 as fib_digits
+
+
+def test_fibonnnaci_digits_math_correct():
+    num_2digit = fib_digits.fibonacci_digits_math(2)
+    assert num_2digit == 7
+
+    num_3digit = fib_digits.fibonacci_digits_math(2)
+    assert num_3digit == 12
