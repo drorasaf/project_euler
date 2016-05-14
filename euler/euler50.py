7ec3b666fb5d1fe60df94e2ae0c8437e9cf6e9ce
Dror <hellfliers@walla.co.il>
2016-05-14 22:30:22 +0300

    change numpy version

--- euler/euler50.py
+++ euler/euler50.py
@@ -1,9 +1,20 @@
 import primesieve
 
 
-def cumulative_primes(max_range):
-    primesum = primesieve.primesum(max_range)
+def generate_primes_sum(primes_array):
+    """ Returns a list of the cumulative sum of all primes """
+    primes_sum = [primes_array[0]]
+    for i in range(1, len(primes_array)):
+        primes_sum.append(primes_array[i] + primes_sum[i-1])
 
+    return primes_sum
+
+
+def search_cumulative_primes(max_range):
+    """ Search for the first cumulative """ 
+    primes = primesieve.generate_primes(max_range)
+    primesum = generate_primes_sum(primes)
+    
 
 def solution():
     """ Returns the answer as requested by the euler project problem """
--- tests/test_euler50.py
+++ tests/test_euler50.py
@@ -1,8 +1,9 @@
 import euler.euler50 as euler
 
 
-def test_euler50():
-    pass
+def test_primes_sum():
+    primes_sum = euler.generate_primes_sum([2, 3, 5, 7, 11, 13])
+    assert primes_sum == [2, 5, 10, 17, 28, 41]
 
 
 def test_solution():
